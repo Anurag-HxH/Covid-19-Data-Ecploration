e4a8df19b0cae3bfdfc9a9b7b332cbc852fa2b3d
Anurag-HxH <139192257+Anurag-HxH@users.noreply.github.com>
2023-09-16 16:02:35 +0530

    Update Data Cleaning Portfolio Project.sql

--- Data Cleaning Portfolio Project.sql
+++ Data Cleaning Portfolio Project.sql
@@ -175,10 +175,10 @@ From PortfolioProject..NashvilleHousing
 
 Update NashvilleHousing
 SET SoldAsVacant = CASE
-						WHEN SoldAsVacant = 'y' THEN 'Yes'
-						WHEN SoldAsVacant = 'N' THEN 'No'
-						ELSE SoldAsVacant
-					END
+			WHEN SoldAsVacant = 'y' THEN 'Yes'
+			WHEN SoldAsVacant = 'N' THEN 'No'
+			ELSE SoldAsVacant
+		   END
 
 
 
@@ -195,16 +195,16 @@ FROM PortfolioProject..NashvilleHousing
 
 With RowNumCTE AS
 (
-Select *,
-ROW_NUMBER() OVER (Partition By ParcelID,
-								PropertyAddress,
-								SaleDate,
-								SalePrice,
-								LandValue,
-								LegalReference
-								Order By
-									UniqueID
-								) row_num
+Select *, ROW_NUMBER() 
+OVER (Partition By ParcelID,
+		   PropertyAddress,
+		   SaleDate,
+		   SalePrice,
+		   LandValue,
+		   LegalReference
+		   Order By
+		       UniqueID
+		   ) row_num
 
 From PortfolioProject..NashvilleHousing
 --Order By ParcelID
@@ -250,52 +250,7 @@ Drop Column OwnerAddress, SaleDate, PropertyAddress, TaxDistrict
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
 
---- Importing Data using OPENROWSET and BULK INSERT	
 
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
-
---GO 
-
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
-
---GO 
-
-
----- Using BULK INSERT
-
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (
---      FIELDTERMINATOR = ',',
---      ROWTERMINATOR = '\n'
---);
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
 
 
 
